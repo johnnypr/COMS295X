f7bccce635dca7f1c49c913cefa03efd2e90b5e7
Johnny Perez <johnnypr11@icloud.com>
2019-05-01 23:31:02 -0500

    p10038

---
+++ p10038.py
@@ -0,0 +1,44 @@
+import sys
+
+
+while (True):
+
+    # User input
+    stdIn = sys.stdin.readline()
+    # Breaks if empty line
+    if stdIn == "\n":
+        break
+    
+    
+    input = stdIn.strip().split()
+    arr = []
+
+    n = 0
+      
+    for x in range(len(input)-1):
+        arr.append(int(input[x+1]))
+        n += 1;
+        
+    diff = 0
+    set =[]
+    
+    for x in range(n-1):
+        set.append(x+1)
+    
+    for x in range(n-1): 
+      
+        if (x == 1):
+            print("Jolly")
+            break
+        
+        diff = abs(arr[x] - arr[x +1])
+        if (diff in set):
+            set.remove(diff)
+            if (len(set)==0):
+                print("Jolly")
+                break
+            else:
+                continue
+        else:   
+            print("Not jolly")
+            break
