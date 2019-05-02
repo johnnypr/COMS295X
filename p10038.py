13137d64cadcf3ab63c9095f75738c87683175e3
Gonzalez-Alfonzo <iagonzal@iastate.edu>
2019-05-02 00:16:53 -0500

    editing

--- p10038.py
+++ p10038.py
@@ -2,43 +2,50 @@ import sys
 
 
 while (True):
+    stdIn = input()
+    
 
-    # User input
-    stdIn = sys.stdin.readline()
-    # Breaks if empty line
-    if stdIn == "\n":
-        break
-    
-    
-    input = stdIn.strip().split()
-    arr = []
 
-    n = 0
-      
-    for x in range(len(input)-1):
-        arr.append(int(input[x+1]))
-        n += 1;
-        
-    diff = 0
-    set =[]
+
+
+
+
+    # # User input
+    # stdIn = sys.stdin.readline()
+    # # Breaks if empty line
+    # if stdIn == "\n":
+    #     break
     
-    for x in range(n-1):
-        set.append(x+1)
     
-    for x in range(n-1): 
+    # input = stdIn.strip().split()
+    # arr = []
+
+    # n = 0
       
-        if (x == 1):
-            print("Jolly")
-            break
+    # for x in range(len(input)-1):
+    #     arr.append(int(input[x+1]))
+    #     n += 1;
         
-        diff = abs(arr[x] - arr[x +1])
-        if (diff in set):
-            set.remove(diff)
-            if (len(set)==0):
-                print("Jolly")
-                break
-            else:
-                continue
-        else:   
-            print("Not jolly")
-            break
+    # diff = 0
+    # set =[]
+    
+    # for x in range(n-1):
+    #     set.append(x+1)
+    
+    # for x in range(n-1): 
+      
+    #     if (x == 1):
+    #         print("Jolly")
+    #         break
+        
+    #     diff = abs(arr[x] - arr[x +1])
+    #     if (diff in set):
+    #         set.remove(diff)
+    #         if (len(set)==0):
+    #             print("Jolly")
+    #             break
+    #         else:
+    #             continue
+    #     else:   
+    #         print("Not jolly")
+    #         break
