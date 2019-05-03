1d885e7dd88f8efa0bccea2e7bea2263eaa59aa9
Johnny Perez <johnnypr11@icloud.com>
2019-05-03 01:02:31 -0500

    waldorf

--- waldorf.java
+++ waldorf.java
@@ -1,9 +1,11 @@
-package unit3;
 import java.util.*;
 import java.io.*;
 
 
 public class waldorf {
+
+
+    //-----------------------------Assume positions-------------------------------------//
     public static int checkFHorizontal(char[][] grid,String word,int x, int y,int rows, int columns){
         int end = 0;
 
@@ -76,7 +78,6 @@ public class waldorf {
 
         
 
-
         if (y<=column-word.length() && x<=row-word.length()){
             for(int i = 1; i<word.length();i++){
                 if (grid[x+i][y+i] != word.charAt(i)){
@@ -90,58 +91,120 @@ public class waldorf {
     }
 
 
-    public static void findWords(char[][] grid,String[] words, int row , int column){
-        
-        for (int index = 0; index < words.length; index++){
-            int wordIndex = index;
-        
-            for(int r = 0; r<row;r++){
-                for(int c= 0; c<column;c++){
-                    if (grid[r][c] == words[wordIndex].charAt(0)){
-                        System.out.println(words[wordIndex]);
-                        if(wordIndex == 1){
-                            System.out.println((r+1) + " " + (c+1));
-                            
-                            continue;
-                        }
-                         if (checkFHorizontal(grid, words[wordIndex], r, c,row,column) != 0){
-                        System.out.println(((r+1) + " " + (c+1)));
-                        continue;
-                        }
-                    
-                        if (checkRHorizontal(grid, words[wordIndex], r, c,row,column) != 0){
-                            System.out.println(((r+1) + " " + (c+1)));
-                            continue;
-                        }
-                    
-                        if (checkVertical(grid, words[wordIndex], r, c,row,column) != 0){
-                            System.out.println(((r+1) + " " + (c+1)));
-                            continue;
-                        }
-                    
-                        if (checkDecline(grid, words[wordIndex], r, c,row,column) != 0){
-                            System.out.println(((r+1) + " " + (c+1)));
-                            continue;
-                        }
-                    
-                        if (checkIncline(grid, words[wordIndex], r, c,row,column) != 0){
-                            System.out.println(((r+1) + " " + (c+1)));
-                            continue;
-                        }
+    //new methods for edge cases
+    public static int checkFIncline(char[][] grid,String word,int x, int y,int row, int column){
+        int top = 0;
 
-                    }
-                }
-           
-            }
         
-            
+
+        if (y>=column-word.length() && x>=row-word.length()){
+            for(int i = 1; i<word.length();i++){
+                if (grid[x+i][y+i] != word.charAt(i)){
+                    return top;
+                }   
+                top = y+i;
+            }
         }
-        return;
+        return top;
+    }
+
+    public static int checkFDecline(char[][] grid,String word,int x, int y,int row, int column){
+        int top = 0;
+
+        if (y>=column-word.length() && x>=row-word.length()){
+            for(int i = 1; i<word.length();i++){
+                if (grid[x-i][y+i] != word.charAt(i)){
+                    return top;
+                }   
+                top = y+i;
+            }
+        }
+        return top;
+
     }
 
 
+    public static int checkFVertical(char[][] grid,String word,int x, int y,int row, int column){
+        int end = 0;
+       
 
-    public static void main(String[] args) throws FileNotFoundException { 
+        if (x<=rows-word.length()){
+            for(int i = 1; i<word.length();i++){
+                if (grid[x+i][y] != word.charAt(i)){
+                    end =0;
+                    return end;
+                }
+                end = y+i;
+            }
+        }
+        return end;
+    }
+
+    //------------------------------find words----------------------------------------//
+
+    public static void findWords(char[][] grid,String[] words, int row , int column){
+        
+
+        search: 
+            for (int index = 0; index < words.length; index++){
+                int wordIndex = index;
+                
+            
+                for(int r = 0; r<row;r++){
+                    for(int c= 0; c<column;c++){
+
+
+                        if (grid[r][c] == words[wordIndex].charAt(0)){
+                            System.out.println(words[wordIndex]);
+                            
+                            if(words[wordIndex].length() <= 1){
+                                System.out.println((r+1) + " " + (c+1));
+                                continue search;
+                            }
+
+
+                            if (checkFHorizontal(grid, words[wordIndex], r, c,row,column) != 0){
+                                System.out.println(((r+1) + " " + (c+1)));
+                                continue search;
+                            
+                            }
+                        
+                            if (checkRHorizontal(grid, words[wordIndex], r, c,row,column) != 0){
+                                System.out.println(((r+1) + " " + (c+1)));
+                                continue search;
+                                
+                            }
+                        
+                            if (checkVertical(grid, words[wordIndex], r, c,row,column) != 0){
+                                System.out.println(((r+1) + " " + (c+1)));
+                                continue search;
+                                
+                            }
+                        
+                            if (checkDecline(grid, words[wordIndex], r, c,row,column) != 0){
+                                System.out.println(((r+1) + " " + (c+1)));
+                                continue search;
+                                
+                            }
+                        
+                            if (checkIncline(grid, words[wordIndex], r, c,row,column) != 0){
+                                System.out.println(((r+1) + " " + (c+1)));
+                                continue search;
+                                
+                            }
+
+                        }
+                    }
+            
+                }
+                
+            }
+            return;
+        }
+
+
+
+public static void main(String[] args) throws FileNotFoundException { 
         File file = new File("input.txt");
         ArrayList<String> data = new ArrayList<String>();
         String line;
@@ -186,19 +249,13 @@ public class waldorf {
             }
 
             
-            
             findWords(grid, words, x, y);
             System.out.println("\n");
-                
-    
-    
-            
+                        
             testcases -=1;
         
         }
         scn.close();
-
-        
 
     }
     
