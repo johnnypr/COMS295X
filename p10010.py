f1e825159be7c109158fcd00fdf0214810a9e8d6
Gonzalez-Alfonzo <iagonzal@iastate.edu>
2019-05-03 01:04:46 -0500

    0.333

--- p10010.py
+++ p10010.py
@@ -25,36 +25,44 @@ import sys
                 
 
 
+# data = []
 
-def readLine():
-    stdIn = sys.stdin.readline()
-    if stdIn=='':
-        return None
-    elif stdIn != '\n':
-        stdIn = stdIn.rstrip().split()
+
+
+
+def readLines():
+    stdIn = input()
+    
+    
     return stdIn
 
 
 if __name__ == '__main__':
-    while True:
-        data = readLine()
-        rows = int(data[1])
-        columns =int(data[2])
-
-        index = 0
-        for x in range(rows):
-            for y in range(columns):
-                grid[x][y] = data[index].lower()
-                index += 1
+    testcase = readLines()
+    print(testcase)
         
-        n = int(data[index+1])
-        words = []
-        for word in range(n):
-            words[word] = data[index + 1]
-            index += 1
+   
+   
+    # while True:
+    #     data = readLine()
+    #     rows = int(data[1])
+    #     columns =int(data[2])
 
-        print(grid)
+
+    #     index = 0
+    #     for x in range(rows):
+    #         for y in range(columns):
+    #             grid[x][y] = data[index].lower()
+    #             index += 1
         
-        if data is None:
-            break
+    #     n = int(data[index+1])
+    #     words = [[]]
+    #     for word in range(n):
+    #         words[word] = data[index + 1]
+    #         index += 1
+
+    #     print(grid)
+        
+    #     if data is None:
+    #         break
         
