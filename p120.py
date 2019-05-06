f5b96b463d36a713b44ea40edd9bb09dda1bb9bd
Gonzalez-Alfonzo <iagonzal@iastate.edu>
2019-05-06 03:42:41 -0500

    p120

--- p120.py
+++ p120.py
@@ -1,9 +1,8 @@
 import sys
 
 
-stack=[]
-n = 0
 pancakes= []
+stack =[]
 
 
 def main():
@@ -11,31 +10,56 @@ def main():
     line = input()
     if line == '':
         break
-    pancakes = line
-    if isSorted(pancakes) == -1:
-        return
-    else:
-        while(isSorted(pancakes) != -1):
-            pushStack(pancakes)
+    
+    for c in list(line):
+        pancakes.append(c)
+    
+    while (isSorted() != -1):
+       pushStack()
+       flip()
+
+   
+       
 
 
-def isSorted(arr):
+def isSorted():
+    global pancakes
     counter = 0;
-    for i in range(len(arr)-1):
-        if arr[i] < arr[i+1]:
+    for i in range(len(pancakes)-1):
+        if pancakes[i] < pancakes[i+1]:
             counter += 1;
         else:
             return i;
-    if i == len(arr) -2:
+    if i == len(pancakes) -2:
         return -1
 
-def pushStack(arr):
-    if isSorted(arr) == 0:
-        for i in range(len(arr)-1):
-            stack.append(arr[i])
+def pushStack():
+    global pancakes
+    if isSorted() == 0:
+        for i in range(len(pancakes)):
+            stack.append(pancakes[i])
     else:
-        for i in range(isSorted(arr), 0):
+        for i in range(isSorted(pancakes), 0):
+            stack.append(pancakes[i])
+        return stack
+        
             
+def flip():
+    global pancakes
+    global stack
+    j = len(stack)-1
+    for i in range(len(stack)):
+        pancakes[i] = stack[j]
+
+        j-=1;
+    
+        
+    stack = []
+      
+
+   
+    
+    
 
 
 
