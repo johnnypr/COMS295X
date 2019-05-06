7afd150464037aae47923ad01f63c9e9f1cae428
Johnny Perez <johnnypr11@icloud.com>
2019-05-05 23:55:56 -0500

    clean

--- p10038.py
+++ p10038.py
@@ -20,7 +20,6 @@ def is_jolly(numbers):
 
 
 if __name__ == '__main__':
-
     while True:
         numbers = load_nums()
         if numbers is None:
--- p120.py
+++ p120.py
@@ -7,19 +7,42 @@ pancakes= []
 
 
 def main():
+ while True:
+    line = input()
+    if line == '':
+        break
+    pancakes = line
+    if isSorted(pancakes) == -1:
+        return
+    else:
+        while(isSorted(pancakes) != -1):
+            pushStack(pancakes)
+
+
+def isSorted(arr):
+    counter = 0;
+    for i in range(len(arr)-1):
+        if arr[i] < arr[i+1]:
+            counter += 1;
+        else:
+            return i;
+    if i == len(arr) -2:
+        return -1
+
+def pushStack(arr):
+    if isSorted(arr) == 0:
+        for i in range(len(arr)-1):
+            stack.append(arr[i])
+    else:
+        for i in range(isSorted(arr), 0):
+            
 
 
 
 
-def invert(start,end):
-    
-
-
-
-
-
-
-
+# def sortLoop(arr):
+#   for i in range(len(arr)-1):
+      
 
 if __name__ == '__main__':
     main()
